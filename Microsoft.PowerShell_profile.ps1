106463ce4e19360a57252883bcd9b0d91f51991f
Diego Bouchet <diegobouchet88@gmail.com>
2022-01-08 15:16:56 +0100

    error handled

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,10 @@
-C:/Users/diego/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+(& "C:\Users\diego\anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+#endregion
+
+# Oh-my-posh
+oh-my-posh --init --shell pwsh --config ~\Desktop\dotfiles\myprompt.json | Invoke-Expression
+
+# Icons
+Import-Module -Name Terminal-Icons
