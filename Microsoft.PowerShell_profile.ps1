ca0cd255c01f988a00e1595d6d96fe483c621c28
Diego Bouchet <diegobouchet88@gmail.com>
2026-01-29 12:35:09 +0100

    final

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,3 @@
-# !! Contents within this block are managed by 'conda init' !!
-#If (Test-Path "C:\Users\diego\miniconda3\Scripts\conda.exe") {
-#    (& "C:\Users\diego\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
-#}
-#endregion
-
 (& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
 (& uvx --generate-shell-completion powershell) | Out-String | Invoke-Expression
 
